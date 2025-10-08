b978a1a4778918d9c23f942e8c4ee9cbe16e56e8
Rajeel Ansari <51192167+rajeeell@users.noreply.github.com>
2025-10-08 19:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (11.91%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,12 +1,11 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        buckets = [0, 0, 0]
-        for color in nums:
-            buckets[color] += 1
+        counts = [0, 0, 0]
+        for n in nums:
+            counts[n] += 1
         
-        offset = 0
-        for color in range(len(buckets)):
-            n = buckets[color]
-            for i in range(offset, offset + n):
-                nums[i] = color
-                offset += 1
+        i = 0
+        for n in range(len(counts)):
+            for j in range(counts[n]):
+                nums[i] = n
+                i += 1
